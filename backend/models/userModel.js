170acc05879ed2505d7742755197311aa28e6493
Atharva Narkhede <atharvan12345@gmail.com>
2024-08-23 07:54:03 +0530

    stable with otp and updated user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,7 +7,7 @@ const passwordHistorySchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   token: { type: String },
